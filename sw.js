a99f13c4486b47c6913f2df9cce775c2baafa229
Lomme89 <95694491+Lomme89@users.noreply.github.com>
2026-05-09 10:24:23 +0200

    Sistemazione cache PWA

--- sw.js
+++ sw.js
@@ -16,9 +16,11 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ includeUncontrolled: true, type: 'window' }))
+      .then(clients => clients.forEach(c => c.postMessage({ type: 'SW_UPDATED', cache: CACHE })))
   );
 });
 
